c1a7891310e7840d50f2ec7c611d3f1d49f17add
Wakate1 <84420283+Wakate1@users.noreply.github.com>
2021-12-28 12:16:32 +0400

    Create generators

---
+++ src/CSRGenerator.java
@@ -0,0 +1,177 @@
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.ContentVerifierProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaContentVerifierProviderBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.PKCSException;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.util.Scanner;
+
+public class CSRGenerator {
+
+
+    public PKCS10CertificationRequest generateCSR(String algorithm, int keySize, SecureRandom random, String signatureAlgorithm) {
+
+
+        //prepare keypair
+        KeyPair keyPair = prepareKeyPair(algorithm, keySize, random);
+
+        //prepare subject details
+        X500Name subject = prepareSubject();
+
+        //prepare signer
+        ContentSigner signer = prepareSigner(signatureAlgorithm, keyPair);
+
+        //create and sign CSR
+        PKCS10CertificationRequestBuilder csrBuilder = new JcaPKCS10CertificationRequestBuilder(subject, keyPair.getPublic());
+        PKCS10CertificationRequest CSR = csrBuilder.build(signer);
+
+
+        return CSR;
+    }
+
+
+    public void exportCSR(PKCS10CertificationRequest CSR, File directory, String csrName) {
+
+        try {
+            FileOutputStream out = new FileOutputStream(directory.getAbsoluteFile() + "\\" + csrName + ".csr");
+            out.write(CSR.getEncoded());
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public boolean CSRisVerified(PKCS10CertificationRequest CSR, PublicKey publicKey) {
+
+        boolean verified = false;
+        try {
+            ContentVerifierProvider verifier = new JcaContentVerifierProviderBuilder().build(publicKey);
+            CSR.isSignatureValid(verifier);
+            verified = true;
+        } catch (OperatorCreationException e) {
+            e.printStackTrace();
+        } catch (PKCSException e) {
+            e.printStackTrace();
+        }
+
+        return verified;
+    }
+
+    private KeyPair prepareKeyPair(String algorithm, int keySize, SecureRandom random) {
+        KeyPairGenerator generator;
+
+        try {
+            random = new SecureRandom();
+            generator = KeyPairGenerator.getInstance(algorithm);
+            generator.initialize(keySize, random);
+            KeyPair pair = generator.generateKeyPair();
+
+            FileOutputStream keyOut;
+
+            //export private key
+            keyOut = new FileOutputStream("privatekey.key");
+            keyOut.write(pair.getPrivate().getEncoded());
+            keyOut.flush();
+
+            //export public key
+            keyOut = new FileOutputStream("publickey.key");
+            keyOut.write(pair.getPublic().getEncoded());
+            keyOut.flush();
+
+            keyOut.close();
+            return pair;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    private X500Name prepareSubject() {
+
+        X500NameBuilder builder = new X500NameBuilder();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the following details (* indicates to a mandatory field) | (enter . if field not needed)");
+        String[] fields = new String[7];
+
+        System.out.print("* Common Name (CN): ");
+        fields[0] = input.nextLine();
+        if (!fields[0].equals("."))
+            builder.addRDN(BCStyle.CN, fields[0]);
+
+
+        System.out.print("Organization (O): ");
+        fields[1] = input.nextLine();
+        if (!fields[1].equals("."))
+            builder.addRDN(BCStyle.O, fields[1]);
+
+
+        System.out.print("Organizational Unit (OU): ");
+        fields[2] = input.nextLine();
+        if (!fields[2].equals("."))
+            builder.addRDN(BCStyle.OU, fields[2]);
+
+
+        System.out.print("City/Locality (L): ");
+        fields[3] = input.nextLine();
+        if (!fields[3].equals("."))
+            builder.addRDN(BCStyle.L, fields[3]);
+
+
+        System.out.print("Country (C): ");
+        fields[4] = input.nextLine();
+        if (!fields[4].equals("."))
+            builder.addRDN(BCStyle.C, fields[4]);
+
+
+        System.out.print("Email Address: ");
+        fields[5] = input.nextLine();
+        if (!fields[5].equals("."))
+            builder.addRDN(BCStyle.CN, fields[5]);
+
+
+        return builder.build();
+    }
+
+
+    private ContentSigner prepareSigner(String signatureAlgorithm, KeyPair keyPair) {
+
+        //create signer
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(signatureAlgorithm);
+        ContentSigner signer = null;
+        try {
+            signer = signerBuilder.build(keyPair.getPrivate());
+            return signer;
+        } catch (OperatorCreationException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+}
---
+++ src/CertificateGenerator.java
@@ -0,0 +1,169 @@
+
+import org.bouncycastle.asn1.ASN1Set;
+import org.bouncycastle.asn1.pkcs.CertificationRequest;
+import org.bouncycastle.asn1.pkcs.CertificationRequestInfo;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class CertificateGenerator {
+
+    public CertificateGenerator() {
+    }
+
+
+    //self-signed generated certificate
+    public X509Certificate generateSelfSignedCert(String keyAlgorithm, String signatureAlgorithm, SecureRandom random, int validityDuration) {
+
+        //generate CSR
+        CSRGenerator csrGenerator = new CSRGenerator();
+        PKCS10CertificationRequest CSR = csrGenerator.generateCSR(keyAlgorithm, 2048, random, signatureAlgorithm);
+
+
+        //setup certificate builder
+        Date notBefore = getDates(validityDuration)[0];
+        Date notAfter = getDates(validityDuration)[1];
+        SubjectPublicKeyInfo keyInfo = CSR.getSubjectPublicKeyInfo();
+        X509v3CertificateBuilder builder = new X509v3CertificateBuilder(CSR.getSubject(), new BigInteger(64, random), notBefore, notAfter, CSR.getSubject(), keyInfo);
+
+
+        //create signer builder
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(signatureAlgorithm);
+        ContentSigner signer = null;
+        X509Certificate certificate = null;
+
+
+        //create and sign certificate
+        try {
+            FileInputStream fis = new FileInputStream("privatekey.key");
+            ;
+            PrivateKey privateKey = KeyFactory.getInstance(keyAlgorithm).generatePrivate(new PKCS8EncodedKeySpec(fis.readAllBytes()));
+
+            signer = signerBuilder.build(privateKey);
+            X509CertificateHolder holder = builder.build(signer);
+            certificate = new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider()).getCertificate(holder);
+        } catch (OperatorCreationException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+        return certificate;
+    }
+
+
+    //issuer-based generated certificate
+    public X509Certificate generateSignedCertificate(PKCS10CertificationRequest CSR, String signatureAlgorithm, SecureRandom random, X500Name issuer, PrivateKey issuerPrivateKey, int duration) {
+
+        //setup certificate builder
+        Date notBefore = getDates(duration)[0];
+        Date notAfter = getDates(duration)[1];
+        SubjectPublicKeyInfo keyInfo = CSR.getSubjectPublicKeyInfo();
+        X509v3CertificateBuilder builder = new X509v3CertificateBuilder(issuer, new BigInteger(64, random), notBefore, notAfter, CSR.getSubject(), keyInfo);
+
+
+        //create signer builder
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(signatureAlgorithm);
+        ContentSigner signer = null;
+        X509Certificate certificate = null;
+
+
+        //create and sign certificate
+        try {
+            signer = signerBuilder.build(issuerPrivateKey);
+            X509CertificateHolder holder = builder.build(signer);
+            certificate = new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider()).getCertificate(holder);
+        } catch (OperatorCreationException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+
+        return certificate;
+    }
+
+
+    public void exportCertificate(Certificate certificate, File directory, String certificateName) {
+        try {
+            FileOutputStream out = new FileOutputStream(directory.getAbsoluteFile() + "\\" + certificateName + ".crt");
+            out.write(certificate.getEncoded());
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean CertificateIsVerified(Certificate certificate, PublicKey publicKey) {
+
+        boolean verified = false;
+
+        try {
+            certificate.verify(publicKey);
+            verified = true;
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        }
+
+        return verified;
+    }
+
+
+    private Date[] getDates(int duration) {
+
+        Date notBefore = new Date(System.currentTimeMillis());
+        long afterValue = TimeUnit.DAYS.toMillis(duration) + notBefore.getTime();
+        Date notAfter = new Date(afterValue);
+
+        return new Date[]{notBefore, notAfter};
+    }
+
+}
